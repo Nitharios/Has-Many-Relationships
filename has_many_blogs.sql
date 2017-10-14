ca5790c431255d9710e941bb0fff4a036f3331da
Nathan N <Nitharios@users.noreply.github.com>
2017-10-14 10:49:22 -1000

    References created

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -9,6 +9,9 @@ CREATE DATABASE has_many_blogs
 -- Before each create table statement, add a drop table if exists statement.
 \c has_many_blogs;
 -- In has_many_blogs.sql Create the tables (including any PKs, Indexes, and Constraints that you may need) to fulfill the requirements of the has_many_blogs schema below.
+
+-- Create the necessary FKs needed to relate the tables according to the relationship table below.
+
 CREATE TABLE users(
   id          SERIAL        NOT NULL    PRIMARY KEY,
   username    varchar(90)   NOT NULL,
@@ -21,6 +24,7 @@ CREATE TABLE users(
 CREATE TABLE posts(
   id          SERIAL        NOT NULL    PRIMARY KEY,
   title       varchar(180),
+  author      integer       NOT NULL    REFERENCES users(id),
   url         varchar(510),
   content     text,
   created_at  timestamptz   NOT NULL    DEFAULT now(),
@@ -29,10 +33,11 @@ CREATE TABLE posts(
 
 CREATE TABLE comments(
   id          SERIAL        NOT NULL    PRIMARY KEY,
+  post        integer       NOT NULL    REFERENCES posts(id),
+  author      integer       NOT NULL    REFERENCES users(id),
   body        varchar(510),
   created_at  timestamptz   NOT NULL    DEFAULT now(),
   updated_at  timestamptz   NOT NULL    DEFAULT now()
 );
 
--- Create the necessary FKs needed to relate the tables according to the relationship table below.
 -- Run the provided scripts/blog_data.sql
