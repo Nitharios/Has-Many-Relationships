46e297b106c6e74db630bbb799edb57e1cc7171b
Nathan N <Nitharios@users.noreply.github.com>
2017-10-14 11:01:21 -1000

    Adding further requirements

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -24,20 +24,50 @@ CREATE TABLE users(
 CREATE TABLE posts(
   id          SERIAL        NOT NULL    PRIMARY KEY,
   title       varchar(180),
-  author      integer       NOT NULL    REFERENCES users(id),
   url         varchar(510),
   content     text,
   created_at  timestamptz   NOT NULL    DEFAULT now(),
   updated_at  timestamptz   NOT NULL    DEFAULT now()
+  user_id     integer       NOT NULL    REFERENCES users(id),
 );
 
 CREATE TABLE comments(
   id          SERIAL        NOT NULL    PRIMARY KEY,
-  post        integer       NOT NULL    REFERENCES posts(id),
-  author      integer       NOT NULL    REFERENCES users(id),
   body        varchar(510),
   created_at  timestamptz   NOT NULL    DEFAULT now(),
   updated_at  timestamptz   NOT NULL    DEFAULT now()
+  post_id     integer       NOT NULL    REFERENCES posts(id),
+  user_id     integer       NOT NULL    REFERENCES users(id),
 );
 
 -- Run the provided scripts/blog_data.sql
+\i scripts/blog_data.sql;
+
+-- Write the following SQL statements in joins.sql
+
+-- Create a query to get all fields from the users table
+
+
+-- Create a query to get all fields from the posts table where the user_id is 100
+
+-- Create a query to get all posts fields, the user's first name, and the user's last name, from the posts table where the user's id is 200
+
+-- Create a query to get all posts fields, and the user's username, from the posts table where the user's first name is 'Norene' and the user's last_name is 'Schmitt'
+
+-- Create a query to get usernames from the users table where the user has created a post after January 1, 2015
+
+-- Create a query to get the post title, post content, and user's username where the user who created the post joined before January 1, 2015
+
+-- Create a query to get the all rows in the comments table, showing post title (aliased as 'Post Title'), and the all the comment's fields
+
+-- Create a query to get the all rows in the comments table, showing post title (aliased as post_title), post url (ailased as post_url), and the comment body (aliased as comment_body) where the post was created before January 1, 2015
+
+-- Create a query to get the all rows in the comments table, showing post title (aliased as post_title), post url (ailased as post_url), and the comment body (aliased as comment_body) where the post was created after January 1, 2015
+
+-- Create a query to get the all rows in the comments table, showing post title (aliased as post_title), post url (ailased as post_url), and the comment body (aliased as comment_body) where the comment body contains the word 'USB'
+
+-- Create a query to get the post title (aliased as post_title), first name of the author of the post, last name of the author of the post, and comment body (aliased to comment_body), where the comment body contains the word 'matrix' ( should have 855 results )
+
+-- Create a query to get the first name of the author of the comment, last name of the author of the comment, and comment body (aliased to comment_body), where the comment body contains the word 'SSL' and the post content contains the word 'dolorum' ( should have 102 results )
+
+-- Create a query to get the first name of the author of the post (aliased to post_author_first_name), last name of the author of the post (aliased to post_author_last_name), the post title (aliased to post_title), username of the author of the comment (aliased to comment_author_username), and comment body (aliased to comment_body), where the comment body contains the word 'SSL' or 'firewall' and the post content contains the word 'nemo' ( should have 218 results )
