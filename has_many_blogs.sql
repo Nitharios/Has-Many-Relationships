22764feb3eb53f0815c43a5e61abb5ca2ded0d1b
Nathan N <Nitharios@users.noreply.github.com>
2017-10-14 10:37:05 -1000

    Able to create tables

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -1,14 +1,38 @@
 \c nniosco;
--- Create a new postgres user named has_many_user
+DROP DATABASE IF EXISTS has_many_blogs;
 DROP USER IF EXISTS has_many_user;
+-- Create a new postgres user named has_many_user
 CREATE USER has_many_user;
 -- Create a new database named has_many_blogs owned by has_many_user
-DROP DATABASE IF EXISTS has_many_blogs;
 CREATE DATABASE has_many_blogs 
   WITH OWNER has_many_user;
 -- Before each create table statement, add a drop table if exists statement.
-
+\c has_many_blogs;
 -- In has_many_blogs.sql Create the tables (including any PKs, Indexes, and Constraints that you may need) to fulfill the requirements of the has_many_blogs schema below.
+CREATE TABLE users(
+  id          SERIAL        NOT NULL    PRIMARY KEY,
+  username    varchar(90)   NOT NULL,
+  first_name  varchar(90),
+  last_name   varchar(90),
+  created_at  timestamp     NOT NULL    DEFAULT now(),
+  updated_at  timestamp     NOT NULL    DEFAULT now()
+);
+
+CREATE TABLE posts(
+  id          SERIAL        NOT NULL    PRIMARY KEY,
+  title       varchar(180),
+  url         varchar(510),
+  content     text,
+  created_at  timestamp     NOT NULL    DEFAULT now(),
+  updated_at  timestamp     NOT NULL    DEFAULT now()
+);
+
+CREATE TABLE comments(
+  id          SERIAL        NOT NULL    PRIMARY KEY,
+  body        varchar(510),
+  created_at  timestamp     NOT NULL    DEFAULT now(),
+  updated_at  timestamp     NOT NULL    DEFAULT now()
+);
 
 -- Create the necessary FKs needed to relate the tables according to the relationship table below.
 -- Run the provided scripts/blog_data.sql
